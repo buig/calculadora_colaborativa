a1b5e9275f8d8e54cf2c09eed8e828729d9a6936
Pau López Núñez <paulopeznunez@gmail.com>
2025-05-14 22:06:11 +0200

    Update test_calculadora.py

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,8 +1,14 @@
 import unittest
-from calculadora import suma  # se irán importando más
+from calculadora import suma, resta  # se irán importando más
 
-class TestCalculadora(unittest.TestCase):
+# Test Función Suma
+class TestSuma(unittest.TestCase):
     def test_suma(self):
         self.assertEqual(suma(2, 3), 5)
-
+        
 # Nuevos tests se añadirán con cada nueva función
+
+# Test Función Resta
+class TestResta(unittest.TestCase):
+    def test_resta(self):
+        self.assertEqual(resta(5, 2), 3)
