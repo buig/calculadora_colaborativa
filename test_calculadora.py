9324f87cf940cfaa21389caa2351350b26b8841a
Sonia Lujan <sonialujangm@gmail.com>
2025-05-15 17:14:40 +0200

    Cambio 1

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,4 +5,8 @@ class TestCalculadora(unittest.TestCase):
     def test_suma(self):
         self.assertEqual(suma(2, 3), 5)
 
+        class TestCalculadora(unittest.TestCase):
+    def test_resta(self):
+        self.assertEqual(suma(3, 2), 1)
+
 # Nuevos tests se añadirán con cada nueva función
